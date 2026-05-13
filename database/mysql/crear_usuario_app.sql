806e00c4152144d91bbc71963686117827b3ccfe
Campbeel <ben3425@gmail.com>
2026-05-13 00:05:58 -0400

    act. y funcnioamiento de base de datos con .venv

--- database/mysql/crear_usuario_app.sql
+++ database/mysql/crear_usuario_app.sql
@@ -1,3 +1,4 @@
 CREATE USER IF NOT EXISTS 'almacen_app'@'localhost' IDENTIFIED BY 'Pama5121';
+ALTER USER 'almacen_app'@'localhost' IDENTIFIED BY 'Pama5121';
 GRANT ALL PRIVILEGES ON almacen_interno.* TO 'almacen_app'@'localhost';
 FLUSH PRIVILEGES;
